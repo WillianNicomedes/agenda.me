43187b8d5b736fa6596a7e2da6dfa5d19cc29eaa
WillianNicomedes <willian.nicomedes1031@gmail.com>
2022-12-06 23:31:52 -0300

    Update estagio.php

--- sis-agendame/estagio.php
+++ sis-agendame/estagio.php
@@ -71,7 +71,6 @@
     <script src="./js/jquery.js"></script>
     <script src="./js/jquery.form.js"></script>
     <script src="./js/upload.js"></script>
-    <script src="./js/bloqueador.js" defer></script>
     <script src="./js/javascript-agendador.js"></script>
 <!-- Option 1: Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
