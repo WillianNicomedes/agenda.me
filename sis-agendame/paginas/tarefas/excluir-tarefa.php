17c29266faffd981ebc4ba59785d04359efd2124
WillianNicomedes <willian.nicomedes1031@gmail.com>
2022-12-11 13:45:21 -0300

    Update excluir-tarefa.php

--- sis-agendame/paginas/tarefas/excluir-tarefa.php
+++ sis-agendame/paginas/tarefas/excluir-tarefa.php
@@ -11,7 +11,7 @@ or die("Erro ao excluir o registro. Erro:" . mysqli_error($conexao) );
   <hr>
   <div>
     <a class="btn btn-outline-danger mb-2" 
-    href="admin.php?menuop=tarefas"><i class="bi bi-list-task"></i> Voltar para a lista de tarefas</a>
+    href="admin.php?menuop=tarefas"><i class="bi bi-list-task"></i> Voltar para tarefas</a>
   </div>
 </div>
   <?php
