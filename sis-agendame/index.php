2a46ba877136d78e766db29e5c927618647c04fa
WillianNicomedes <willian.nicomedes1031@gmail.com>
2022-12-10 21:40:31 -0300

    Update index.php

--- sis-agendame/index.php
+++ sis-agendame/index.php
@@ -17,14 +17,13 @@
     <title>Sistema agenda.me</title>
 </head>
 <body>
-    <header class="bg-dark">
-        <div class="container">
-            <nav class="navbar navbar-expand-lg  navbar-dark bg-dark">
+<header class="bg-dark">
+            <nav class="navbar navbar-expand-lg  navbar-dark" style="background-color: #135B7D;">
+                <a href="#" class="navbar-brand"></a>
                 <a href="#" class="navbar-brand">
                     <img src="./img/logo.png" alt="sis-agenda.me" width="130" >
                 </a>
             </nav>
-        </div>
     </header>
     <main>
    <div class="container">
